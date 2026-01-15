683fa6afb949f580f90184b8de7be3c400cd9d2d
arunchandv-py <arunchand.v@gmail.com>
2026-01-15 17:34:20 +0530

    log error
    .

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 console.log('Index.js is running');
+console.log('Current environment:', process.env.NODE_ENV);
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/mood-tracker">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
